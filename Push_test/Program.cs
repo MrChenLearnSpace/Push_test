b127149e8b8d128d4cd92251fe69cf23cb735e57
PC Error <58879669+MrChenLearnSpace@users.noreply.github.com>
2025-06-07 07:15:59 +0000

    在 Program.cs 中增加了一次输出 "github action" 的语句

--- Push_test/Program.cs
+++ Push_test/Program.cs
@@ -6,5 +6,6 @@ class Program
         Console.WriteLine("Hello, World!");
         Console.WriteLine("github action");
         Console.WriteLine("github action");
+        Console.WriteLine("github action");
     }
 }
